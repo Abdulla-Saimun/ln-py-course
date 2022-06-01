bad770efbd4db2d4aec4f5351afcd09d3555204a
Abdulla-Saimun <abdullah35-1921@diu.edu.bd>
2022-06-01 21:54:00 +0600

    abstract and interface

--- inheritance/interfaces.py
+++ inheritance/interfaces.py
@@ -3,6 +3,11 @@
 from abc import ABC, abstractclassmethod
 from turtle import circle
 
+class JSONify(ABC):
+    @abstractclassmethod
+    def toJSON(self):
+        pass
+
 class GraphicShape:
     def __init__(self):
         super().__init__()
@@ -11,23 +16,18 @@ class GraphicShape:
     def calcArea(self):
         return 'hello'
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
+
     def calcArea(self):
         return 3.14*self.radius**2 
 
+    def toJSON(self):
+        return f"{{ \"circle\": {str(self.calcArea())}  }}"   #conver to json
 
-class Square(GraphicShape):
-    def __init__(self, side):
-        self.side = side
-    def calcArea(self):
-        return self.side*self.side
+    
 
-
-g = GraphicShape()
-print(g.calcArea)
-c = Circle(10)
-print(c.calcArea())
-s = Square(12)
-print(s.calcArea())
+c1 = Circle(10)
+print(c1.calcArea())
+print(c1.toJSON())
